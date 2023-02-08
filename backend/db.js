a4f5cb3406327baeb2ea1957b9a542f10a1c3692
Prince2319 <piyush19.09yadav@gmail.com>
2023-02-08 17:42:59 +0530

    First Commit

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook"
+
+mongoose.set('strictQuery', false);
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI , ()=>{
+        console.log("Connected to mongo succefully");
+    });
+}
+
+module.exports = connectToMongo;
